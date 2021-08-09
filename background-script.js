41bcc2021c706409663d56c9e2ca1bfb6e268f17
Elouan Martinet <exa@elou.world>
2021-08-09 21:57:46 +0200

    Save last created tab if last active got closed
    In case of the new tab is to be opened in another container, a new tab
    is created just after, so the last active tab points to the closed tab.

--- background-script.js
+++ background-script.js
@@ -3,6 +3,11 @@ let lastActiveTab = {
 	"pinned": false
 };
 let loadInBackground = true;
+let lastCreatedTab = {
+	"id": -1,
+	"index": -1,
+	"opener": -1
+};
 
 browser.storage.local.get("loadInBackground").then(function(res) {
 	if (res.loadInBackground === undefined) {
@@ -32,13 +37,28 @@ browser.tabs.onActivated.addListener(function(activeInfo) {
 });
 
 browser.tabs.onCreated.addListener(function(tab) {
+	if (lastActiveTab.pinned == false
+		&& tab.openerTabId == lastCreatedTab.opener
+		&& tab.id == lastCreatedTab.id + 1
+		&& tab.index == lastCreatedTab.index + 1
+	) {
+		lastActiveTab = {
+			"id": lastCreatedTab.opener,
+			"pinned": true
+		}
+	}
 	if (lastActiveTab.pinned) {
 		browser.tabs.query({
 			"currentWindow": true,
 			"pinned": true
 		})
 		.then(function(tabs) {
-			if (tab.index == tabs.length) {
+			if (tab.index == tabs.length || tab.index == tabs.length + 1) {
+				lastCreatedTab = {
+					"id": tab.id,
+					"index": tab.index,
+					"opener": tab.openerTabId
+				};
 				browser.tabs.move(tab.id, {
 					index: -1
 				})
